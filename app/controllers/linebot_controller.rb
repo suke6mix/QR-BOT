e21634fb05c8a54935361e0a3adf0752ac36d113
Yosuke MIKASA <ohayo.mylon66@gmail.com>
2020-12-22 18:52:47 +0900

    LINE BOTのオウム返し実装完了

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -15,7 +15,6 @@ class LinebotController < ApplicationController
 
     def client
         @client ||= Line::Bot::Client.new { |config|
-            #config.channel_id = ENV["LINE_CHANNEL_ID"]
             config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
             config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
         }
